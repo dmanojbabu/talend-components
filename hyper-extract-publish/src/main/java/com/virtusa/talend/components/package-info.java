3482f4b9b4e797fe07bd94f16b01aceacc86ac90
Manoj <manoj.deenathayalamani@nielsen.com>
2020-04-01 14:31:22 +0530

    component name changes

--- hyper-extract-publish/src/main/java/com/virtusa/talend/components/package-info.java
+++ hyper-extract-publish/src/main/java/com/virtusa/talend/components/package-info.java
@@ -1,7 +1,7 @@
 // this tells the framework in which family (group of components) and categories (UI grouping)
 // the components in the nested packages belong to
-@Components(family = "tTableau", categories = "Business Intelligence")
-@Icon(value = CUSTOM, custom = "tTableau") // icon is located at src/main/resources/icons/tTableau.svg
+@Components(family = "Tableau", categories = "Business Intelligence")
+@Icon(value = CUSTOM, custom = "t") // icon is located at src/main/resources/icons/tTableau.svg
 package com.virtusa.talend.components;
 
 import static org.talend.sdk.component.api.component.Icon.IconType.CUSTOM;
